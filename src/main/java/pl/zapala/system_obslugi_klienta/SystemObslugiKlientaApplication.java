b195df4d45488b01609bb24a8a340cd90d05e2c4
kubazap <jz.jakub.zapala@gmail.com>
2025-03-24 16:13:45 +0100

    Szkielet aplikacji

---
+++ src/main/java/pl/zapala/system_obslugi_klienta/SystemObslugiKlientaApplication.java
@@ -0,0 +1,13 @@
+package pl.zapala.system_obslugi_klienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemObslugiKlientaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemObslugiKlientaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zapala/system_obslugi_klienta/SystemObslugiKlientaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zapala.system_obslugi_klienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemObslugiKlientaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
