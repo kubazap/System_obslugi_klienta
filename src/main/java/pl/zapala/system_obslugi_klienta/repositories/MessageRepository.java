f6b485d2b90c697e182468995e8a6673c41e52d2
Maciej <miudi12732@gmail.com>
2025-05-06 03:51:41 +0200

    Implementacja repozytorium klasy Message

---
+++ src/main/java/pl/zapala/system_obslugi_klienta/repositories/MessageRepository.java
@@ -0,0 +1,21 @@
+package pl.zapala.system_obslugi_klienta.repositories;
+
+import pl.zapala.system_obslugi_klienta.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    @Query("SELECT m FROM Message m WHERE " +
+            "(m.senderId = :user1 AND m.receiverId = :user2) OR " +
+            "(m.senderId = :user2 AND m.receiverId = :user1) " +
+            "ORDER BY m.sentAt ASC")
+    List<Message> findConversationBetweenUsers(@Param("user1") Integer user1, @Param("user2") Integer user2);
+
+    List<Message> findBySenderIdOrderBySentAtDesc(Integer senderId);
+
+    List<Message> findByReceiverIdOrderBySentAtDesc(Integer receiverId);
+}
