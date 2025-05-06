18633f8d37b77b09990485af879c193c7b8431e4
kubazap <jz.jakub.zapala@gmail.com>
2025-05-06 12:13:23 +0200

    Poprawki testów

--- src/test/java/pl/zapala/system_obslugi_klienta/EmailTest.java
+++ src/test/java/pl/zapala/system_obslugi_klienta/EmailTest.java
@@ -5,13 +5,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import pl.zapala.system_obslugi_klienta.validators.EmailValidator;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.mail.username=dummy",
+        "spring.mail.password=dummy",
+        "spring.mail.host=localhost",
+        "emails.sender_email=dummy@example.com"})
 class EmailTest {
     private EmailValidator validator;
     @BeforeEach
--- src/test/java/pl/zapala/system_obslugi_klienta/SystemObslugiKlientaApplicationTests.java
+++ src/test/java/pl/zapala/system_obslugi_klienta/SystemObslugiKlientaApplicationTests.java
@@ -1,9 +1,21 @@
 package pl.zapala.system_obslugi_klienta;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.mail.username=dummy",
+		"spring.mail.password=dummy",
+		"spring.mail.host=localhost",
+		"emails.sender_email=dummy@example.com"})
 class SystemObslugiKlientaApplicationTests {
 
 	@Test
